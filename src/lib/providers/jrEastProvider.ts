27fbd70c214acc391a1845d3d3f74a56dc7745b7
주영준(mts2497) <gmarketit01@gmarket.com>
2026-08-31 16:01:43 +0900

    feat: add JR East Tokaido timetable support

--- src/lib/providers/jrEastProvider.ts
+++ src/lib/providers/jrEastProvider.ts
@@ -10,6 +10,7 @@ const railwayMap: Record<string, string> = {
   "chuo-sobu": "odpt.Railway:JR-East.ChuoSobuLocal",
   saikyo: "odpt.Railway:JR-East.SaikyoKawagoe",
    "shonan-shinjuku": "odpt.Railway:JR-East.ShonanShinjuku",
+   tokaido: "odpt.Railway:JR-East.Tokaido",
 };
 
 type OdptStationTimetableObject = {
